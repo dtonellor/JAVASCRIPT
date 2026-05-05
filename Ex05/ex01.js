246ac37346b8fb78ac4cfda562b4f742a8317615
Daniel Tonello R. <dtonellor99@gmail.com>
2026-05-05 18:34:07 -0300

    exs de JS

---
+++ Ex05/ex01.js
@@ -0,0 +1,8 @@
+var idade = 19
+if (idade < 16) {
+    console.log(`não vota.`)
+} else if (idade < 18) {
+        console.log(`voto opcional.`)
+} else if (idade > 18) {
+    console.log('Voto Obrigatório.')
+}
